5e25a55722282f47141f376d17b5b39ea606aff5
klsharma20121997 <klsharma.20121997@gmail.com>
2025-08-31 23:26:58 +0530

    Header Changed

--- js/main.js
+++ js/main.js
@@ -13,13 +13,13 @@
 
 
     // Sticky Navbar
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 45) {
-            $('.navbar').addClass('sticky-top shadow-sm');
-        } else {
-            $('.navbar').removeClass('sticky-top shadow-sm');
-        }
-    });
+    // $(window).scroll(function () {
+    //     if ($(this).scrollTop() > 45) {
+    //         $('.navbar').addClass('sticky-top shadow-sm');
+    //     } else {
+    //         $('.navbar').removeClass('sticky-top shadow-sm');
+    //     }
+    // });
 
 
     // International Tour carousel
